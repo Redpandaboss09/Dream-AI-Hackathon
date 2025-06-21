fdbd73018f95c888117dad8e255c0a0bff2cdb30
Johan <almanzar.j@northeastern.edu>
2025-06-21 16:32:04 -0400

    Nothing to see here, just some setup ;p

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import render_template, Flask
 
 app = Flask(__name__)
 
-"""
+
 @app.route('/')
 def index():
     return render_template('index.html')
-"""
 
-@app.route('/')
+
+@app.route('/map')
 def map_view():
     return render_template('map.html')
